ae7091a0a0303c8851a0c35cbf38ef2c6395590c
Jacgarvel <jacobo.estudiante16@gmail.com>
2022-02-28 19:05:47 +0100

    Lectura de ficheros y sha256

--- function.py
+++ function.py
@@ -1,19 +1,18 @@
 import os
 import hashlib
+from traceback import print_tb
 
-ejemplo_dir = 'C:/Users/jcmor/Documents/SSII/SecurityTeam17/DATA/'
+ejemplo_dir = '.\\DATA'
 
 #Creamos esta funcion para recorrer todas las carpetas del directorio 
 # y obtener así una lista de todos los ficheros que contienen
 def search_files (directorio):
     result = []
-    for nombre_directorio, dirs, ficheros in os.walk(ejemplo_dir):
-        print(dirs)
+    for nombre_directorio, dirs, ficheros in os.walk(ejemplo_dir):       
         for nombre_fichero in ficheros:
-            result.append(nombre_directorio+nombre_fichero)
+            result.append(nombre_directorio+ "\\" +nombre_fichero)
     return result
 
-'''
 def getsha256file(directorio):
     lista = search_files (directorio)
     for file in lista:
@@ -32,9 +31,9 @@ def getsha256file(directorio):
         except:
             print("Error desconocido")
             return ""
-'''
+
 
 if __name__ == '__main__':
     search_files(ejemplo_dir)
-   # print(getsha256file('C:/Users/jcmor/Documents/SSII/SecurityTeam17/DATA/'))
+    print(getsha256file('.\\DATA'))
     
