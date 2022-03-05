d435b18bc2c85e9d1a487fdc53c52a153d5b6e5f
Antonio Manuel <antomanu2000@hotmail.es>
2022-03-05 20:22:02 +0100

    Añadido una función HMAC

--- function.py
+++ function.py
@@ -1,12 +1,21 @@
 import os
+import hmac
 import hashlib
 import uuid
 import sqlite3
+
 
 ejemplo_dir = '.\\DATA'
 
 #Creamos esta funcion para recorrer todas las carpetas del directorio 
 # y obtener así una lista de todos los ficheros que contienen
+def calc_hmac(clave, mensaje):
+    clave = bytes(clave, 'utf-8')
+    mensaje = bytes(mensaje, 'utf-8')
+    res = hmac.new(clave, mensaje, hashlib.sha256)
+    
+    return res.hexdigest()
+
 def search_files (directorio):
     result = []
     for nombre_directorio, dirs, ficheros in os.walk(ejemplo_dir):       
@@ -45,6 +54,7 @@ def getsha256file(directorio):
 
 
 if __name__ == '__main__':
-    search_files(ejemplo_dir)
-    getsha256file('.\\DATA')
-    
+    #search_files(ejemplo_dir)
+    #getsha256file('.\\DATA')
+    print(calc_hmac('clavisima', 'mensaje ocult'))
+    print(calc_hmac('clavisima', 'mensaje oculto'))
