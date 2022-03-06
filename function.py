32c7b1a2f496ce25399c4b01b8bbd15e5f99b87c
JoseCarlosMorales <josmorbor3@alum.us.es>
2022-03-06 12:51:27 +0100

    challenge creado

--- function.py
+++ function.py
@@ -3,19 +3,15 @@ import hmac
 import hashlib
 import uuid
 import sqlite3
+import random
+import string
 
 
 ejemplo_dir = '.\\DATA'
 
+
 #Creamos esta funcion para recorrer todas las carpetas del directorio 
 # y obtener así una lista de todos los ficheros que contienen
-def calc_hmac(clave, mensaje):
-    clave = bytes(clave, 'utf-8')
-    mensaje = bytes(mensaje, 'utf-8')
-    res = hmac.new(clave, mensaje, hashlib.sha256)
-    
-    return res.hexdigest()
-
 def search_files (directorio):
     result = []
     for nombre_directorio, dirs, ficheros in os.walk(ejemplo_dir):       
@@ -30,7 +26,8 @@ def getsha256file(directorio):
     conn.execute('''create table ficheros(
     fichero     text not null,
     hash        text not null,
-    token       text not null);''')
+    token       text not null,
+    challenge   text not null);''')
     try:
         for file in lista:
             #print (file)
@@ -39,7 +36,8 @@ def getsha256file(directorio):
             with open(file, "rb") as f:
                 for bloque in iter(lambda: f.read(4096), b""):
                     hashsha.update(bloque)
-            conn.execute('''insert into ficheros values (?,?,?)''',(file,hashsha.hexdigest(),token))   
+                    challenge = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(6))
+            conn.execute('''insert into ficheros values (?,?,?,?)''',(file,hashsha.hexdigest(),token,challenge))   
             
         conn.commit()
         print(str(conn.execute("SELECT * FROM ficheros").fetchall()))
@@ -52,9 +50,15 @@ def getsha256file(directorio):
         print("Error desconocido")
         return ""
 
+def calc_hmac(clave, mensaje):
+    clave = bytes(clave, 'utf-8')
+    mensaje = bytes(mensaje, 'utf-8')
+    res = hmac.new(clave, mensaje, hashlib.sha256)
+    
+    return res.hexdigest()
 
 if __name__ == '__main__':
     #search_files(ejemplo_dir)
-    #getsha256file('.\\DATA')
-    print(calc_hmac('clavisima', 'mensaje ocult'))
-    print(calc_hmac('clavisima', 'mensaje oculto'))
+    getsha256file('.\\DATA')
+    #print(calc_hmac('clavisima', 'mensaje ocult'))
+    #print(calc_hmac('clavisima', 'mensaje oculto'))
