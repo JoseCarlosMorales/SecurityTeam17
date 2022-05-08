edb5d63dc49bfe9a4ca4a4d1f2981d8965e9913b
Jacgarvel <jacobo.estudiante16@gmail.com>
2022-05-08 11:13:37 +0200

    Initial Commitç

--- DATA_CLIENT/decide/store/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-
-urlpatterns = [
-    path('', views.StoreView.as_view(), name='store'),
-    path('store-bot/', views.StoreBotView.as_view()),
-]
